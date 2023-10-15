22fc5026287644f7a8bd32c27e1028fbc76b4cfe
IDOShun <ido_shun@retail-ai.jp>
2023-10-15 09:01:07 +0000

    [add] stream operations

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
+import io
+import os
+from dotenv import load_dotenv
+
 from googleapiclient.discovery import build
 from httplib2 import Http
 from oauth2client import file, client, tools
+from googleapiclient.http import MediaIoBaseDownload
 
 from google.cloud import storage
 
 
 # drive
 SCOPES = ['https://www.googleapis.com/auth/drive.readonly']
-CREDENTIAL_OAUTH = './credential_OAuth.json'
-FOLDER_ID = '1wdiC8e3fKpDkvdIQvf_9-63fIeDCsxR_'
+CREDENTIAL_OAUTH = os.getenv('CREDENTIAL_OAUTH')
+FOLDER_ID = os.getenv('FOLDER_ID')
 
 # gcs
-PROJECT_ID = 'sandbox-ido'
-CREDENTIAL_GCS = './credential_gcs_sa.json'
-BUCKET_NAME = 'camera_data_rcv_test_bucket'
+PROJECT_ID = os.getenv('PROJECT_ID')
+CREDENTIAL_GCS =  os.getenv('CREDENTIAL_GCS')
+BUCKET_NAME = os.getenv('BUCKET_NAME')
 
 
 def authorizeApi(scopes, credential_path):
@@ -24,20 +29,18 @@ def authorizeApi(scopes, credential_path):
         creds = tools.run_flow(flow, store)
     return build('drive', 'v3', http=creds.authorize(Http()))
 
-drive_service = authorizeApi(SCOPES, CREDENTIAL_OAUTH)
-
-def getFilesFromGDrive(folder_id, file_name, isMyDrive):
-    # file_name = 'test'
+def getFilesFromGDrive(folder_id, filter_by_filename, isMyDrive):
     filters = [
         f"'{folder_id}' in parents",
-        f"name contains '{file_name}'",
+        f"name contains '{filter_by_filename}'",
         "mimeType != 'application/vnd.google-apps.folder'"
     ]
     query = " and ".join(filters)
 
     folders = drive_service.files().list(
         q=query,
-        spaces= "root" if isMyDrive else "drive",
+        # spaces= "root" if isMyDrive else "drive",
+        spaces= "drive",
         includeItemsFromAllDrives= False if isMyDrive else True,
         supportsAllDrives=True,
         fields='nextPageToken, files(id, name)'
@@ -46,25 +49,34 @@ def getFilesFromGDrive(folder_id, file_name, isMyDrive):
 
     if not files_list:
         print(f'No files found in folder with ID {folder_id}.')
-    else:
-        print(f"Files in the folder with ID {folder_id}:")
-        for file in files_list:
-            print(f"{file['name']} ({file['id']})")
+    # else:
+    #     print(f"Files in the folder with ID {folder_id}:")
+    #     for file in files_list:
+    #         print(f"{file['name']} ({file['id']})")
+    return files_list
+
+def uploadFilesToGCS(file_name, file_id, credential, bucket_name):
+    request = drive_service.files().get_media(fileId=file_id)
+    file_stream = io.BytesIO()
+    downloader = MediaIoBaseDownload(file_stream, request)
+    done = False
+    while not done:
+        _, done = downloader.next_chunk()
+    file_stream.seek(0)
+
+    # Upload file stream to Google Cloud Storage
+    storage_client = storage.Client.from_service_account_json(credential)
+    bkt = storage_client.bucket(bucket_name)
+    blob = bkt.blob(file_name)
+    print(f"uploading {file_name}... It may take long time.\n")
+    blob.upload_from_file(file_stream, content_type='video/mp4',timeout=600)
+
+    print(f"File from Google Drive uploaded to GCS at: gs://{bucket_name}/{file_name}")
 
 
-# upload to gcs
-storage_client = storage.Client.from_service_account_json(CREDENTIAL_GCS)
-# buckets = list(storage_client.list_buckets())
-# print(buckets)
+# Main Operation Below
+drive_service = authorizeApi(SCOPES, CREDENTIAL_OAUTH)
+filelists = getFilesFromGDrive(FOLDER_ID, "", True)
 
-# Create a test file
-# with open('./test.txt', 'w') as f:
-#     f.write('This is a test file for GCS!')
-
-bucket = storage_client.bucket(BUCKET_NAME)
-blob = bucket.blob('test.txt')
-
-with open('./test.txt', 'rb') as f:
-    blob.upload_from_file(f)
-
-# Upload the test file to GCS
+for f in filelists :
+    uploadFilesToGCS(f['name'], f['id'], CREDENTIAL_GCS, BUCKET_NAME)
