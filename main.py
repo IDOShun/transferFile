920dd499af8ac9ceefef8ff6416af80e7f4534bf
IDOShun <ido_shun@retail-ai.jp>
2023-10-15 07:17:43 +0000

    [add] get files and upload files

--- main.py
+++ main.py
@@ -2,53 +2,69 @@ from googleapiclient.discovery import build
 from httplib2 import Http
 from oauth2client import file, client, tools
 
+from google.cloud import storage
+
+
+# drive
 SCOPES = ['https://www.googleapis.com/auth/drive.readonly']
+CREDENTIAL_OAUTH = './credential_OAuth.json'
+FOLDER_ID = '1wdiC8e3fKpDkvdIQvf_9-63fIeDCsxR_'
 
-store = file.Storage('token.json')
-creds = store.get()
-if not creds or creds.invalid:
-    flow = client.flow_from_clientsecrets('./client_secret.json', SCOPES)
-    creds = tools.run_flow(flow, store)
-drive_service = build('drive', 'v3', http=creds.authorize(Http()))
+# gcs
+PROJECT_ID = 'sandbox-ido'
+CREDENTIAL_GCS = './credential_gcs_sa.json'
+BUCKET_NAME = 'camera_data_rcv_test_bucket'
 
 
+def authorizeApi(scopes, credential_path):
+    store = file.Storage('token.json')
+    creds = store.get()
+    if not creds or creds.invalid:
+        flow = client.flow_from_clientsecrets(credential_path, scopes)
+        creds = tools.run_flow(flow, store)
+    return build('drive', 'v3', http=creds.authorize(Http()))
 
-# About Google Drive
-FOLDER_ID = ""
-# About GCS Info
-GCS_PROJECT_ID = ""
-BUCKET_NAME = ""
-SA_CRED = ""
-SA_EMAIL = ""
+drive_service = authorizeApi(SCOPES, CREDENTIAL_OAUTH)
 
-def myFunction() :
-  transferDriveFilesToGCS(FOLDER_ID)
+def getFilesFromGDrive(folder_id, file_name, isMyDrive):
+    # file_name = 'test'
+    filters = [
+        f"'{folder_id}' in parents",
+        f"name contains '{file_name}'",
+        "mimeType != 'application/vnd.google-apps.folder'"
+    ]
+    query = " and ".join(filters)
 
-def transferDriveFilesToGCS(folderId, bucketName, projectId, serviceAccount):
-  folder = DriveApp.getFolderById(folderId)
-  files = folder.getFiles()
-  
-  while (files.hasNext()):
-    file = files.next()
-    blob = file.getBlob()
-    # Upload to GCS
-    uploadToGCS(blob, file.getName(), bucketName, projectId, serviceAccount)
+    folders = drive_service.files().list(
+        q=query,
+        spaces= "root" if isMyDrive else "drive",
+        includeItemsFromAllDrives= False if isMyDrive else True,
+        supportsAllDrives=True,
+        fields='nextPageToken, files(id, name)'
+        ).execute()
+    files_list = folders.get('files', [])
 
-def uploadToGCS(blob, fileName, bucketName, projectId, serviceAccount):
-  url = "https://storage.googleapis.com/upload/storage/v1/b/" + bucketName + "/o?uploadType=media&name=" + fileName
-  
-    headers = {
-    "Authorization": "Bearer " + ScriptApp.getOAuthToken(),
-    "x-goog-project-id": projectId
-    }
-   options = {
-    "method": "POST",
-    "contentType": blob.getContentType(),
-    "payload": blob.getBytes(),
-    "headers": headers,
-    "muteHttpExceptions": true
-  }
+    if not files_list:
+        print(f'No files found in folder with ID {folder_id}.')
+    else:
+        print(f"Files in the folder with ID {folder_id}:")
+        for file in files_list:
+            print(f"{file['name']} ({file['id']})")
 
-  response = UrlFetchApp.fetch(url, options)
-  Logger.log(response)
 
+# upload to gcs
+storage_client = storage.Client.from_service_account_json(CREDENTIAL_GCS)
+# buckets = list(storage_client.list_buckets())
+# print(buckets)
+
+# Create a test file
+# with open('./test.txt', 'w') as f:
+#     f.write('This is a test file for GCS!')
+
+bucket = storage_client.bucket(BUCKET_NAME)
+blob = bucket.blob('test.txt')
+
+with open('./test.txt', 'rb') as f:
+    blob.upload_from_file(f)
+
+# Upload the test file to GCS
